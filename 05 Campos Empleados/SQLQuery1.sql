991f36a27bc3d7911f6c92f48cd284e9ec940395
Matias Gigena <mati-mangekyou@hotmail.com>
2025-01-20 18:06:15 -0300

    Renombramiento de campos Empleados

---
+++ 05 Campos Empleados/SQLQuery1.sql
@@ -0,0 +1,22 @@
+select * from Empleados;
+
+alter table Empleados add sexo char(1);
+alter table Empleados add fe_contratacion date;
+--Resumen: alter table Empleados add sexo char(1), fe_contratacion date;
+
+insert into empleados values (1, 'Juan', 'Pérez', 25, 1234567890, 'Calle 123', '1978-06-15', 2500.00, 'SI','H','1980-05-09');
+
+alter table Empleados drop column fe_contratacion;
+alter table Empleados drop column sexo;
+
+exec sp_rename 'Empleados.idempleado','id';
+
+exec sp_rename 'Empleados.id','ID';
+exec sp_rename 'Empleados.nombre','NOMBRE';
+exec sp_rename 'Empleados.apellido','APELLIDO';
+exec sp_rename 'Empleados.edad','EDAD';
+exec sp_rename 'Empleados.telefono','TELEFONO';
+exec sp_rename 'Empleados.direccion','DIRECCION';
+exec sp_rename 'Empleados.fecha_nacimiento','FECHA_NACIMIENTO';
+exec sp_rename 'Empleados.salario','SALARIO';
+exec sp_rename 'Empleados.activo','ACTIVO';
