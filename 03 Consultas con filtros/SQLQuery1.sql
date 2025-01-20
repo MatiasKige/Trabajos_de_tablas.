798642fd9c4a4b474b258f14a73f522eb1816ff2
Matias Gigena <mati-mangekyou@hotmail.com>
2025-01-20 00:41:23 -0300

    Consultas where

---
+++ 03 Consultas con filtros/SQLQuery1.sql
@@ -0,0 +1,9 @@
+
+select * from Empleados;
+
+--Filtramos por filas.
+select * from Empleados where nombre = 'Jorge';
+
+select * from Empleados where edad = 29;
+
+select nombre,apellido,edad from Empleados where edad = 29;
