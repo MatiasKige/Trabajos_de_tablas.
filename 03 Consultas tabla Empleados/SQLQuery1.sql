546396628090b991569d250b69aa61cd70508b94
Matias Gigena <mati-mangekyou@hotmail.com>
2025-01-20 17:23:31 -0300

    Consultas a Empleados con operadores relacionales

--- 03 Consultas tabla Empleados/SQLQuery1.sql
+++ 03 Consultas tabla Empleados/SQLQuery1.sql
@@ -1,4 +1,3 @@
-
 select * from Empleados;
 
 --Filtramos por filas.
@@ -7,3 +6,16 @@ select * from Empleados where nombre = 'Jorge';
 select * from Empleados where edad = 29;
 
 select nombre,apellido,edad from Empleados where edad = 29;
+
+select * from Empleados where idempleado = 3;
+
+select * from Empleados where edad != 25; --<>
+
+select * from Empleados where salario > 2000;
+
+select * from Empleados where salario < 3000;
+
+select * from Empleados where salario <= 2500;
+
+
+
